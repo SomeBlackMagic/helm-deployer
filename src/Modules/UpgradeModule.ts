8ffc781749741b8740a7e07796b8bf6e476e7f45
Andru Cherny <wiroatom@gmail.com>
2022-11-16 11:50:09 +0200

    debug in progress

--- src/Modules/UpgradeModule.ts
+++ src/Modules/UpgradeModule.ts
@@ -6,12 +6,19 @@ import {clearTimeout} from 'timers';
 const cliColor = require('cli-color');
 
 export class UpgradeModule {
+    private isExit: boolean = false;
     private subProcesses: { [n: string]: ChildProcessWithoutNullStreams } = {};
     private realiseName: string = '';
     private timeouts: NodeJS.Timeout[] = [];
     private intervals: any[] = [];
 
     public async run(cliArgs: string[]) {
+        const test: string[] | null = null;
+        setTimeout(() => {
+            test.forEach(() => {
+                console.log('!');
+            });
+        }, 3000);
         let BreakException = {};
 
         try {
@@ -39,6 +46,7 @@ export class UpgradeModule {
     }
 
     public async stop(): Promise<any> {
+        this.isExit = true;
         this.timeouts.forEach((item) => {
             clearTimeout(item);
         });
@@ -48,13 +56,20 @@ export class UpgradeModule {
         let promises = Object.entries(this.subProcesses).map((entry) => {
             const [key, item] = entry;
             return new Promise((resolve, reject) => {
+                // https://github.com/kubernetes/kubectl/blob/652881798563c00c1895ded6ced819030bfaa4d7/pkg/util/interrupt/interrupt.go#L28
+                item.kill('SIGTERM');
+                const interval = setInterval(() => {
+                    // https://github.com/kubernetes/kubectl/blob/652881798563c00c1895ded6ced819030bfaa4d7/pkg/util/interrupt/interrupt.go#L28
+                    item.kill('SIGTERM');
+                }, 1000);
                 const timer = setTimeout(() => {
+                    clearInterval(interval);
                     console.log('Stop process ' + key + ' timeout. Killing');
                     item.kill('SIGKILL');
                 }, 5000);
-                // https://github.com/kubernetes/kubectl/blob/652881798563c00c1895ded6ced819030bfaa4d7/pkg/util/interrupt/interrupt.go#L28
-                item.kill('SIGTERM');
+
                 item.on('exit', (code: number) => {
+                    clearInterval(interval);
                     clearTimeout(timer);
                     resolve(code);
                 });
@@ -98,11 +113,13 @@ export class UpgradeModule {
                             }
                         });
                     }
-                    podItem.status.containerStatuses.forEach((container) => {
-                        if (container.state.running !== undefined) {
-                            this.kubectlWatchPodContainerLogs(podItem.metadata.name, container.name);
-                        }
-                    });
+                    if (podItem.status.containerStatuses !== undefined) {
+                        podItem.status.containerStatuses.forEach((container) => {
+                            if (container.state.running !== undefined) {
+                                this.kubectlWatchPodContainerLogs(podItem.metadata.name, container.name);
+                            }
+                        });
+                    }
                 });
             })();
 
@@ -168,6 +185,10 @@ export class UpgradeModule {
 
 
     private async createChildProcess(command: string, args: string[], wait: boolean = false, grabStdOut: boolean = false, pipeLogs: boolean = false, logPrefix: string = '', logColor: string = 'white') {
+        if (this.isExit === true) {
+            // console.log('Application is in exit process. Skip create new process');
+            return Promise.resolve();
+        }
         if (this.subProcesses[logPrefix.replace(/\s/g, '-')]) {
             return Promise.resolve(true);
         }
